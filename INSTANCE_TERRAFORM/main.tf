1c03d5131392b55de3bd00f3fc65fb985bd28402
nagswe <126598198+nagswe@users.noreply.github.com>
2024-06-11 07:10:44 +0530

    Update main.tf

---
+++ INSTANCE_TERRAFORM/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0e001c9271cf7f3b9"
+  instance_type = "t2.micro"
+  key_name = "virginanewaccount"
+  availability_zone = "us-east-1b"
+  tags= {
+    Name = "Nagesh"
+  }
+}
